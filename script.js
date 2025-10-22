795e10ab3f73d8b3d5fb24eaf6773183aac97de5
MyGrpMaker <49886997+mygrp1@users.noreply.github.com>
2025-10-22 16:10:33 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Student data array (will be added by user)
   const data = [
+    { MAT: "232331603603", NAME: "MERHOUM", PNAME: "MERIEM", SECT: "13", GRP_TP: "D" },
     { MAT: "222231694213", NAME: "BOUREZG", PNAME: "AGHILES", SECT: "13", GRP_TP: "Unknown" },
     { MAT: "242431653209", NAME: "ZERROUK", PNAME: "MANEL", SECT: "13", GRP_TP: "F" },
     { MAT: "252531643819", NAME: "ABBES", PNAME: "ABDEL MOUMENE", SECT: "13", GRP_TP: "B" },
@@ -2074,4 +2075,5 @@ function getNextLabDateSmart(labType, grpTP) {
 function isABC(grpTP) {
     return ["A", "B", "C"].includes(grpTP);
 }
+
 
